df00a5998f7c5248709f70d52d7b359c652fe45b
Simone Crocco <41231090+Simonotos@users.noreply.github.com>
2023-02-15 12:04:08 +0100

    15 February
    - added environment sound
    - added sound to buttons of  rubik's cube minigame
    - reduced lever activation ranged from rubik's cube minigame

--- MyFirstVrProject/Assets/Scripts/Rubik/ButtonManager.cs
+++ MyFirstVrProject/Assets/Scripts/Rubik/ButtonManager.cs
@@ -4,12 +4,19 @@ public class ButtonManager : MonoBehaviour
 {
     [SerializeField]
     private Animator myAnim;
+    private AudioSource myAudioSource;
 
     [SerializeField]
     private string typeOfBtn;
 
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
     private void onButtonPressed()
     {
+        myAudioSource.Play();
         myAnim.SetTrigger("onPressed");
         RubikSelectFace.rubikSelectFaceInstance.buttonPressed(typeOfBtn);
     }
